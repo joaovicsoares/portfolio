7e03c84cab773dccc34d837a3df636e3b7090234
João Victor Soares <135281593+joaovicsoares@users.noreply.github.com>
2024-08-05 20:10:08 -0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-
+import React from 'react'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-      <h1>portfolio</h1>
+      <Header></Header>
     </>
   )
 }
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,52 @@
+function Header() {
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg ">
+        <div className="container-fluid ">
+          <a className="navbar-brand" href="#">
+          DevJoao
+          </a>
+          <div>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">
+                      Inicio
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      sobre
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      Projetos
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      Contato
+                    </a>
+                  </li>
+                </ul>
+              </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
