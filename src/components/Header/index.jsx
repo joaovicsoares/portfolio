b1ec22ac9e56c86209e0c72657b1b1cc6f4c30e5
João Victor Soares <135281593+joaovicsoares@users.noreply.github.com>
2024-08-06 20:15:51 -0300

    theme-settings

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header>
-      <nav className="navbar navbar-expand-lg ">
+      <nav className="navbar navbar-expand-lg">
         <div className="container-fluid ">
           <a className="navbar-brand" href="#">
           DevJoao
@@ -21,7 +21,7 @@ function Header() {
               <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                   <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">
+                    <a className="nav-link" aria-current="page" href="#">
                       Inicio
                     </a>
                   </li>
